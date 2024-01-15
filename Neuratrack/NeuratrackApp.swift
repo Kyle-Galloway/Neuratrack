2f20dcde272fec44c7a6662d66d992e67af8556f
Kyle Galloway <kyle_galloway@hotmail.co.uk>
2024-01-15 21:56:34 +0000

    Initial Commit

---
+++ Neuratrack/NeuratrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuratrackApp.swift
+//  Neuratrack
+//
+//  Created by Kyle Galloway on 15/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct NeuratrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
